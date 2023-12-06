7ff03fd5e70f3df44d9f1509a3b57cd0e30aafe8
Vishnu Thangaraj <vishnuthangaraj.private@gmail.com>
2023-12-06 09:54:13 +0530

    initial commit

---
+++ src/main/java/com/vishnuthangaraj/MusifyBackendAPI/MusifyBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.vishnuthangaraj.MusifyBackendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusifyBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusifyBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishnuthangaraj/MusifyBackendAPI/MusifyBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnuthangaraj.MusifyBackendAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusifyBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
