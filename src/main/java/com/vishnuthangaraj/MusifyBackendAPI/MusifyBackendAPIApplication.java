55d0e51a7347c825df225647248e2aa6994bdb98
Vishnu Thangaraj <vishnuthangaraj.private@gmail.com>
2023-12-06 09:59:19 +0530

    minor modification

--- src/main/java/com/vishnuthangaraj/MusifyBackendAPI/MusifyBackendAPIApplication.java
+++ src/main/java/com/vishnuthangaraj/MusifyBackendAPI/MusifyBackendAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusifyBackendApiApplication {
+public class MusifyBackendAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusifyBackendApiApplication.class, args);
+		SpringApplication.run(MusifyBackendAPIApplication.class, args);
 	}
 
 }
